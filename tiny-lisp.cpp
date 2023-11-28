45b198f717226c5ea05657feebbd924ae29cccfd
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 11:48:06 +0100

    dentro de la clase variante hago una publica

--- tiny-lisp.cpp
+++ tiny-lisp.cpp
@@ -6,3 +6,14 @@
 #include <iomanip>
 
 class Variant {
+public:
+    using ValueType = std::variant<int, double, std::string, std::vector<Variant>, std::map<std::string, Variant>>;
+
+    // Constructor que acepta un valor inicial
+    Variant(const ValueType& value) : value_(value) {}
+
+    // Método para obtener el valor interno
+    ValueType getValue() const {
+        return value_;
+    }
+};
